954cb25a562e9ef70ba1687a6a6da12fe6bcde72
Christian <chsanleo@gmail.com>
2020-05-31 22:37:03 +0200

    feature: Operations
    finish

--- JS/main.js
+++ JS/main.js
@@ -1,42 +1,76 @@
+window.onload = function(){ cleanLocalStorage();}
 
-//
-window.onload = function(){
-    localStorage.setItem('result','empty');
-}
-
-var screen = document.getElementById("screen");
-var history = document.getElementById("history");
-
-
+const screen = document.getElementById("screen");
+const history = document.getElementById("history");
+var newOperation = false;
 
 function addNumber(number){
+    if(newOperation){
+        screen.innerHTML='';
+        newOperation = false;
+    }
     screen.innerHTML = screen.innerHTML + number;
-    localStorage.setItem('number',number);
-};
+    let numberLS = localStorage.getItem('number');
 
+    if(numberLS=='empty'){ localStorage.setItem('number', number);}
+    else{ localStorage.setItem('number', numberLS + number);}
+}
 
 function addOperation(operation){
+
     let result = localStorage.getItem('result');
     let number = localStorage.getItem('number');
+    let operationLs = localStorage.getItem('operation');
 
-    if(isNaN(result)){
+    if(result == 'empty'){
         localStorage.setItem('result', screen.innerHTML);
+        localStorage.setItem('number','empty');
+        localStorage.setItem('operation', operation);
     }
     else{
-        let num1 = Number(result);
-        let num2 = Number(number);
-
-        switch(operation){
-            case '+': localStorage.setItem('result', num1 + num2); break;
-            case '-': localStorage.setItem('result', num1 - num2); break;
-            default: return; 
-        }
+        operate(operationLs,Number(result),Number(number));
+        if(operation == '='){
+            addResult();
+            newOperation = true;
+        }      
     }
-    screen.innerHTML = screen.innerHTML + operation;
-    history.innerHTML = history.innerHTML + screen.innerHTML;
-};
+    localStorage.setItem('number','empty');
+    if((operation !== '=')){
+        screen.innerHTML = screen.innerHTML + operation;
+        history.innerHTML = history.innerHTML + screen.innerHTML;
+    }
+}
+
+function operate(operation,num1,num2){
+    switch(operation){
+        case '+': localStorage.setItem('result', num1 + num2); break;
+        case '-': localStorage.setItem('result', num1 - num2); break;
+        case '*': localStorage.setItem('result', num1 * num2); break;
+        case '/': if(num2==0){ alert('No se puede dividir por 0'); }
+                  localStorage.setItem('result', num1 / num2);
+                  break;
+        default: return; 
+    }
+}
+
+function addResult(){
+    let result = localStorage.getItem('result');
+
+    if(result !== 'empty'){
+         screen.innerHTML = result; 
+         history.innerHTML = history.innerHTML + localStorage.getItem('number') +' = '+ localStorage.getItem('result');
+        }
+    cleanLocalStorage();
+}
 
 function cleanCalc(){
-    screen.innerHTML ='';
+    screen.innerHTML = '';
+    cleanLocalStorage();
+};
+
+function cleanLocalStorage(){
     localStorage.setItem('result', 'empty');
+    localStorage.setItem('number', 'empty');
+    localStorage.setItem('operation', 'empty');
+    history.innerHTML = history.innerHTML + '<br>';
 };
